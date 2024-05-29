7f419cb7aba889fd2356b54a50bdb576fc6281b0
Kyle Brackman <kyle.a.brackman@gmail.com>
2024-05-29 10:55:03 -0600

    Add cors and correct to show notes and not customers on the backend.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Note = require('./models/note');
+const cors = require('cors');
 
 mongoose.set('strictQuery', false);
 const app = express();
@@ -8,6 +9,8 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
